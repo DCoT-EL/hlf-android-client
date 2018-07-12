b582dbf8f78f1f9304b1ca19dbadd4da5b193146
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-07-12 12:00:03 +0200

    Deploy procedure for library

--- hlf-android-client/deploy.sh
+++ hlf-android-client/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm ./build/outputs/jar/hlf-android-client-debug.zip
-rm -rf ./build/outputs/jar/hlf-android-client-debug
+rm ./build/outputs/jar/hlf-android-client-release.zip
+rm -rf ./build/outputs/jar/hlf-android-client-release
 rm ./build/outputs/jar/hlf-android-client.jar
-./gradlew
-cp ./build/outputs/aar/hlf-android-client-debug.aar ./build/outputs/jar/hlf-android-client-debug.zip
-unzip  -o ./build/outputs/jar/hlf-android-client-debug.zip -d ./build/outputs/jar/hlf-android-client-debug
-mv  ./build/outputs/jar/hlf-android-client-debug/classes.jar  ./build/outputs/jar/hlf-android-client.jar
-cp  ./build/outputs/jar/hlf-android-client.jar ~/Documents/Sviluppo/workspace_dcot/MyApplication/app/libs/hlf-android-client.jar
+mkdir ./build/outputs/jar
+./gradlew assembleRelease
+cp ./build/outputs/aar/hlf-android-client-release.aar ./build/outputs/jar/hlf-android-client-release.zip
+unzip  -o ./build/outputs/jar/hlf-android-client-release.zip -d ./build/outputs/jar/hlf-android-client-release
+mv  ./build/outputs/jar/hlf-android-client-release/classes.jar  ./build/outputs/jar/hlf-android-client.jar
+cp  ./build/outputs/jar/hlf-android-client.jar ../MyApplication/app/libs/hlf-android-client.jar
